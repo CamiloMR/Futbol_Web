77d9eeffc95f84a8550ce657c7b2a73d3583aff6
CamiloMR <camilo98.2254@gmail.com>
2019-04-24 18:35:41 -0500

    V4

---
+++ Backend/server.js
@@ -0,0 +1,8 @@
+const app = require('./app')
+
+app.set('port', process.env.PORT || 3001)
+
+const server = app.listen(app.get('port'), () => {
+    console.log('El servidor está en:')
+    console.log('http://localhost:' + server.address().port)
+})
